a80afe0597e170d93f14106e0a978e9b593ded27
MS-Kwak <kkminseo@gmail.com>
2025-05-03 16:54:30 +0900

    prod일때 constants.js API_URL 조건분기 추가

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,1 +1,6 @@
-export const API_URL = 'http://localhost:3000';
+// export const API_URL = 'http://localhost:3000';
+// https://ethan-market-server.fly.dev
+export const API_URL =
+  import.meta.env.NODE_ENV === 'production'
+    ? 'https://ethan-market-server.fly.dev'
+    : 'http://localhost:3000';
