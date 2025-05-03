8e82f6a8ae2a8d2e6293918b14617d7740d4cbcf
MS-Kwak <kkminseo@gmail.com>
2025-05-03 17:12:37 +0900

    prod일때 constants.js API_URL vite 수정

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,5 @@
 // export const API_URL = 'http://localhost:3000';
 // https://ethan-market-server.fly.dev
-export const API_URL =
-  import.meta.env.NODE_ENV === 'production'
-    ? 'https://ethan-market-server.fly.dev'
-    : 'http://localhost:3000';
+export const API_URL = import.meta.env.PROD
+  ? 'https://ethan-market-server.fly.dev'
+  : 'http://localhost:3000';
